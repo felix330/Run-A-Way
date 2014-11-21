f5ae2a8f8c7aec660369f42b5ef723527b978326
felix330 <felix330@gmail.com>
2014-11-21 23:45:38 +0100

    Added classes

--- BibAway/BibAway/Header.h
+++ BibAway/BibAway/Header.h
@@ -1,2 +1,58 @@
 
 void blabl();
+
+class structure //Für die Speicherung von Gegneransammlungen erbt enemy?
+{
+private:
+
+public:
+
+};
+
+class enemy
+{
+private:
+
+public:
+
+};
+
+class level //erbt structure?
+{
+private:
+
+public:
+
+};
+
+class draw //Zeigt Bildschirminhalt
+{
+private:
+
+public:
+
+};
+
+class powerup
+{
+private:
+
+public:
+
+};
+
+class game
+{
+private:
+
+public:
+
+};
+
+class playercontrol
+{
+private:
+
+public:
+
+};
