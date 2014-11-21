3036418c627ea46da89af35c800c1916c019dcaf
felix330 <felix330@gmail.com>
2014-11-21 17:31:07 +0100

    It fucking works!!! lib_add

--- Run_A_Way/Run_A_Way/Main.cpp
+++ Run_A_Way/Run_A_Way/Main.cpp
@@ -9,56 +9,10 @@
 
 
 #include <SFML/Graphics.hpp>
+#include <header.h>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML Test!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-
-	int count = 0;
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		{
-			int posX = shape.getPosition().x;
-			
-			count++;
-			if (count == 10)
-			{
-				shape.setPosition((posX - 1), 0);
-				count = 0;
-			}
-
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		{
-			int posX = shape.getPosition().x;
-
-			count++;
-			if (count == 10)
-			{
-				shape.setPosition((posX + 1), 0);
-				count = 0;
-			}
-
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-
-	
-
-	return 0;
+	blabl();
+	return 1;
 }
